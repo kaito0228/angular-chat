45445d178ee4825c2e11e2bcc722cbaf156c5d74
ryo.s <aqua.timez.saiko@gmail.com>
2018-11-13 00:14:39 +0900

    modify commentDate to japanese

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { CommentDatePipe } from './pipe/comment-date.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CommentDatePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
